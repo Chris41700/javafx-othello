ad4c9238aaa8407dd1eb82e558823923c8d0ef9a
Chris41700 <ChrisHui417@gmail.com>
2022-05-11 17:10:11 -0400

    Pieces can be added to the board

--- JavaFx-Othello/src/application/GameBoard.java
+++ JavaFx-Othello/src/application/GameBoard.java
@@ -15,8 +15,9 @@ import javafx.scene.control.Button;
 
 public class GameBoard implements isIllegalMove {
 	private static final int GRID_SIZE = 8;
-	private static final int CELL_SIZE = 90;
+	private static final int CELL_SIZE = 110;
 	private static final int PLAYER_SIZE = 2;
+	private boolean whiteTurn = false;				// to check the turn
 	
 	Parent createContent() {
 		GridPane root = new GridPane();
@@ -26,8 +27,8 @@ public class GameBoard implements isIllegalMove {
 
 		//Add Black and White Chip images
 		Image blackChip = new Image("C:\\Users\\Chris\\git\\javafx-othello\\JavaFx-Othello\\src\\application\\Black Chip.png");
-		Image whiteChip = new Image("C:\\Users\\Chris\\git\\javafx-othello\\JavaFx-Othello\\src\\application\\White Chip.png");
-		
+        Image whiteChip = new Image("C:\\Users\\Chris\\git\\javafx-othello\\JavaFx-Othello\\src\\application\\White Chip.png");
+	
 		//Creates the background
 		root.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
 		root.setGridLinesVisible(true);
@@ -48,27 +49,42 @@ public class GameBoard implements isIllegalMove {
 		//Add a button to each rows and columns
 		for (int row = 0; row < GRID_SIZE; row++) {
 			for (int col = 0; col < GRID_SIZE; col++) {
-				Button b = new Button();
+				Button button = new Button();
 				
 				//Set width and height to fit the cell
-				b.setPrefWidth(85);
-				b.setPrefHeight(85);
+				button.setPrefWidth(85);
+				button.setPrefHeight(85);
 				
 				//Check the position if there is a circle cause we don't want a button there
-				b.setVisible(true);
 				
+				//Setting the background of the button to green so that they are "invisible"
+				button.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
+			
 				//Add a function to the button so it let's the user place a chip
-				b.setOnAction(new EventHandler<ActionEvent>() {
+				button.setOnAction(new EventHandler<ActionEvent>() {
 				    @Override
 				    public void handle(ActionEvent e) {
 				    	//Want to find the position of the button and add the image of that position
 				    	//Check the color of the image that we should put
-				    	root.add(new ImageView(blackChip), 4, 6);
+				    	if(whiteTurn == false) {
+				    		ImageView black = new ImageView(blackChip);
+				    		button.setGraphic(black);
+				    		whiteTurn = true;
+				    	}
+				    	else {
+				    		button.setGraphic(new ImageView(whiteChip));
+				    		whiteTurn = false;
+				    	}
+				    	// we don't want this button to work again because every spot can be hit only once
+				    	button.setDisable(true);
+				    	
+				    	// but we don't want the color of the button to change so we set the opacity
+				    	button.setOpacity(10);
 				    }
 				});
-				
 				//Add button to every cell
-				root.add(b, row, col);
+				root.add(button, row, col);
+			
 			}
 		}
 		
